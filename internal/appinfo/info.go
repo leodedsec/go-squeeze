20afa370f10f87fa77113f7798b69309da707945
leodedsec <azat.abdrashitov7@gmail.com>
2026-02-10 17:39:43 +0300

    feat: appinfo

---
+++ internal/appinfo/info.go
@@ -0,0 +1,8 @@
+package appinfo
+
+const (
+	Name        = "go-squeeze"
+	Description = "Create an archive from files in a directory"
+	Version     = "0.1"
+	Github      = "https://github.com/leodedsec/go-squeeze"
+)
